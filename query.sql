34273d2f990377ab625c392a50165d8eab02e195
Mohamed Amine <nafiai.medamine@gmail.com>
2025-12-03 16:17:18 +0100

    select without condition

--- query.sql
+++ query.sql
@@ -4,3 +4,15 @@ VALUES ('John Doe', 'john@example.com', '0612345678', '2025-01-01');
 UPDATE customers
 SET phone = '0700000000'
 WHERE id = 1;
+
+SELECT * FROM customers;
+
+
+SELECT full_name, email FROM customers;
+
+SELECT * FROM accounts;
+
+SELECT account_number FROM accounts;
+
+
+SELECT * FROM transactions;

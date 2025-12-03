794ac9e8d9aa352c7b31d2f853e9bb75f2479aa6
Mohamed Amine <nafiai.medamine@gmail.com>
2025-12-03 16:16:45 +0100

    update

--- query.sql
+++ query.sql
@@ -1,2 +1,6 @@
 INSERT INTO customers (full_name, email, phone, registration_date)
 VALUES ('John Doe', 'john@example.com', '0612345678', '2025-01-01');
+
+UPDATE customers
+SET phone = '0700000000'
+WHERE id = 1;
